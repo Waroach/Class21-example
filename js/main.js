8686c46776a495a906bcbc05fc6c83a2863b9161
Brent Rankin <brent.rankin@gmail.com>
2021-05-06 00:05:56 -0700

    Update main.js
    added a rep get

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//***************************************************************************************
+// let insertURL = `https//www.myreposite.com/`
+// fetch(InsertURL)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     //console log the data from repo
+//     console.log(data)
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   })
